e1d35cd2e4f13ff8ae2c6f3906944de66fb05082
Saksham <skshm.090898@gmail.com>
2024-05-28 19:26:59 +0530

    completed

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+}); 
+
+
+app.listen(3000, function(){
+    console.log("Server Initiated");
+});
+
+app.get('/style.css', function (req, res) {
+    res.sendFile(__dirname + "/" + "style.css");
+});
+
+app.get('/script.js', function (req, res) {
+    res.sendFile(__dirname + "/" + "script.js");
+});
